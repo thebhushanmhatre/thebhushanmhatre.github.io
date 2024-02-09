f9080c188dd1e774531769d8c55d96387cdc12c1
Bhushan C. Mhatre <44663470+thebhushanmhatre@users.noreply.github.com>
2024-02-09 23:35:59 +0530

    stuck after deleting a lot

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
 # thebhushanmhatre.github.io
+
 My Portfolio
 https://thebhushanmhatre.github.io/
+
+# Projects:
+
+- href
+- img
+- description
+- name
+- visible
+- target
+
+# Certificates
+
+- href
+- img
+- name
+- issuer
+- institute
+- professor
+- description
+- visible
+- target
+
+# Contacts
+
+- href
+- name
+
+# Education
+
+- href
+- name
+- shortname
+- img
+- year
+- degree
--- scripts/app.js
+++ scripts/app.js
@@ -6,91 +6,99 @@ toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active');
 });
 
-// To segregate projects - outer layer
-/*
-<div class="projsection text-center">
-  <h4>Data Science based Projects</h4>
-  <div class="projminisection">
-    <!-- projects belonging to above category -->
-  </div>
-</div>
-*/
+const pages = ['projects', 'certificates', 'contacts', 'education'];
 
-// projects
-let projects = document.getElementsByClassName('projminisection')[0];
-let proj_div = ({ href, target, src, name }) =>
-  `<div class="proj text-center">
-    <a  href="${href}" target="${target}">
-      <img class="img-tile" src="${src}" alt="${name}">
-      <br><br>
-      <span class="projname"> ${name} </span>
-    </a>
-  </div>`;
+pages.forEach((page) => {
+  document.getElementById(page + '-btn').addEventListener('click', () => {
+    showPage(page);
+  });
+});
 
-// To segregate certificates - outer layer
-/*
-<div class="certisection text-center mt-4">
-  <h4> Coursera Certificates</h4>
-  <div class="certiminisection">
-    <!-- certificates belonging to above category -->
-  </div>
-</div>
-*/
+function showPage(pageToBeLoaded) {
+  document.getElementById(pageToBeLoaded).style.display = 'show';
+  document.getElementById(pageToBeLoaded + '-btn').style.display = 'none';
+  pages
+    .filter((page) => page != pageToBeLoaded)
+    .forEach((page) => {
+      document.getElementById(page).style.display = 'none';
+      document.getElementById(page + '-btn').style.display = 'block';
+    });
+}
 
-// certificates
-let certificates = document.getElementsByClassName('certiminisection')[0];
-let cert_div = ({ href, target, src, name, institute }) =>
-  `<div class="text-center certbor">
-    <a  href="${href}" target="${target}">
-      <img class="img-tile" src="${src}" alt="${name}">
-      <br>
-      <span class="certname"> ${name} ${
-    institute ? ' : ' + institute : ''
-  } </span>
-      <br>
-    </a>
-  </div>`;
+// Fetch Data
+import allData from './data';
 
-// contacts
-let contacts = document.getElementById('con');
-let contact_div = ({ href, target, color, icon }) =>
-  `<div class="conta">
-    <a class="madhe" href="${href}" target="${target}">
-      <i style="color: ${color}" class="${icon}"></i>
-    </a>
-  </div>`;
+console.log('allData: ', allData);
 
-// education
-let education = document.getElementById('institutions');
-let education_div = ({ href, name, shortname, src, year, degree }) =>
-  `<div class="text-center proj">
-    <a  href="${href}" target="_blank">
-      <img class="img-tile" src="${src}" alt="${name}">
-      <br>
-      <div class="projname"> ${name} ${shortname} </div>
-      <span class="projname"> ${degree} ${year} </span>
-      <br>
-    </a>
-  </div>`;
-
+// Combine HTML with Data
 const display_data = (data) => {
-  projects.innerHTML = data.projects
-    .filter((project) => project.visible)
-    .map((project) => proj_div(project))
-    .join(' ');
-
-  certificates.innerHTML = data.certificates
-    .filter((certificate) => certificate.visible)
-    .map((certificate) => cert_div(certificate))
-    .join(' ');
-
-  contacts.innerHTML = data.contacts.map((item) => contact_div(item)).join(' ');
-
-  education.innerHTML = data.education
-    .map((item) => education_div(item))
-    .join(' ');
+  // contentDiv.innerHTML
+  for (let page of pages) {
+    addContentDiv(data[page]);
+  }
 };
 
-fetch('scripts/data.json')
-  .then((response) => response.json())
-  .then((json) => display_data(json));
+// Create generic layout for different pages:
+// projects, certificates, education, contact and work experience
+let contentDiv = document.getElementById('context');
+let addContentDiv = (details) => console.log(details);
+// `<div class="">${JSON.stringify(details)}</div>`;
+
+// ===================================================================
+
+// // projects
+// let projects = document.getElementsByClassName('projminisection')[0];
+// let proj_div = ({ href, target, src, name }) =>
+//   `<div class="proj text-center">
+//     <a  href="${href}" target="${target}">
+//       <img class="img-tile" src="${src}" alt="${name}">
+//       <br><br>
+//       <span class="projname"> ${name} </span>
+//     </a>
+//   </div>`;
+
+// // To segregate certificates - outer layer
+// /*
+// <div class="certisection text-center mt-4">
+//   <h4> Coursera Certificates</h4>
+//   <div class="certiminisection">
+//     <!-- certificates belonging to above category -->
+//   </div>
+// </div>
+// */
+
+// // certificates
+// let certificates = document.getElementsByClassName('certiminisection')[0];
+// let cert_div = ({ href, target, src, name, institute }) =>
+//   `<div class="text-center certbor">
+//     <a  href="${href}" target="${target}">
+//       <img class="img-tile" src="${src}" alt="${name}">
+//       <br>
+//       <span class="certname"> ${name} ${
+//     institute ? ' : ' + institute : ''
+//   } </span>
+//       <br>
+//     </a>
+//   </div>`;
+
+// // contacts
+// let contacts = document.getElementById('con');
+// let contact_div = ({ href, target, color, icon }) =>
+//   `<div class="conta">
+//     <a class="madhe" href="${href}" target="${target}">
+//       <i style="color: ${color}" class="${icon}"></i>
+//     </a>
+//   </div>`;
+
+// // education
+// let education = document.getElementById('institutions');
+// let education_div = ({ href, name, shortname, src, year, degree }) =>
+//   `<div class="text-center proj">
+//     <a  href="${href}" target="_blank">
+//       <img class="img-tile" src="${src}" alt="${name}">
+//       <br>
+//       <div class="projname"> ${name} ${shortname} </div>
+//       <span class="projname"> ${degree} ${year} </span>
+//       <br>
+//     </a>
+//   </div>`;
